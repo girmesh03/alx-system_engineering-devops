58d8ad3c806a1c7874db6444c731a9b2b645834c
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-21 20:27:01 +0300

    1-install_a_package.pp lint bug fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,4 @@
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep Version | grep 2.1.0',
-  require => Package['python3-pip'],
+package {'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
