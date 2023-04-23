d4e796c8f663d69304be45c1e740995eb66ac3a3
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-23 07:33:19 +0300

    100-puppet_ssh_config.pp pass changed 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,11 +5,9 @@
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/sshd_config',
   line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication\s+ no'
 }
 
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile\s+.+$'
 }
