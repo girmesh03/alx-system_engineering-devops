764d2f2faf7abb4b852aa95cde28eec0f4eb2f3c
girmesh03 <girmachewzewdie03@gmail.com>
2023-04-28 12:57:21 +0300

    100-puppet_ssh_config.pp indentation added

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,11 +3,11 @@
 # The ssh client configuration
 
 file_line{'Turn off passwd authentication':
-path => '/etc/ssh/ssh_config',
-line => 'PasswordAuthentication no'
+    path => '/etc/ssh/ssh_config',
+    line => 'PasswordAuthentication no'
 }
 
 file_line{'Define identity file':
-path => '/etc/ssh/ssh_config',
-line => 'IdentityFile ~/.ssh/school'
+    path => '/etc/ssh/ssh_config',
+    line => 'IdentityFile ~/.ssh/school'
 }
