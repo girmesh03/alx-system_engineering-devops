72a7f2d4d0bc340655fdbfdfa60b73600ac3e09f
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-01 05:28:51 +0300

    2-puppet_custom_http_response_header.pp new update 3

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,19 @@
 # creating a custom HTTP header response using Puppet.
 
 exec {'update':
-    command => 'apt-get update',
-    path    => '/usr/bin',
+  command => '/usr/bin/apt-get update',
 }
 
-  package { 'nginx':
-      ensure => present,
-  }
+package { 'nginx':
+  ensure  => present,
+}
 
-  file_line { 'http_header':
-      path   => '/etc/nginx/nginx.conf',
-      match  => 'http {',
-      line   => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-      notify => Exec['nginx_restart'],
-  }
+file_line { 'header':
+    path  => '/etc/nginx/nginx.conf',
+    match => '^http {',
+    line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
 
-  exec { 'nginx_restart':
-      command     => '/usr/sbin/service nginx restart',
-      refreshonly => true,
-  }
+exec { 'nginx_restart':
+  command => '/usr/sbin/service nginx restart',
+}
