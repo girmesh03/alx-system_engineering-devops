46375eae110f12823b86ba994fa08b0faa2148e5
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-01 05:38:25 +0300

    2-puppet_custom_http_response_header.pp new update 5

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,17 +3,14 @@
 exec {'update':
   command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure  => present,
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file_line { 'header':
-    path  => '/etc/nginx/nginx.conf',
-    match => 'http {',
-    line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-exec { 'nginx_restart':
+-> exec {'run':
   command => '/usr/sbin/service nginx restart',
 }
