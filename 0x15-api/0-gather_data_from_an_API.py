95613cd6ec3dddd7858c5f2d6c02dbee19a476b9
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-23 00:03:06 +0300

    0-gather_data_from_an_API.py update 4

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,21 +3,42 @@
 import requests
 import sys
 
-if __name__ == "__main__":
+
+def fetch_user_todos(employee_id):
     base_url = 'https://jsonplaceholder.typicode.com/'
-    user_url = f"{base_url}users/{sys.argv[1]}"
-    todos_url = f"{base_url}todos?userId={sys.argv[1]}"
-    user_name = requests.get(user_url).json()['name']
-    todos_data = requests.get(todos_url).json()
-    completed = 0
-    total = 0
-    completed_tasks = []
-    for task in todos_data:
-        total += 1
-        if task['completed']:
-            completed_tasks.append(task['title'])
-            completed += 1
+    user_url = f"{base_url}users/{employee_id}"
+    todos_url = f"{base_url}todos"
+
+    response = requests.get(user_url)
+    response.raise_for_status()
+    user_data = response.json()
+
+    response = requests.get(todos_url, params={"userId": employee_id})
+    response.raise_for_status()
+    todos_data = response.json()
+
+    return user_data, todos_data
+
+
+def print_todo_progress(user_data, todos_data):
+    completed_tasks = [task['title']
+                       for task in todos_data if task['completed']]
+    completed = len(completed_tasks)
+    total = len(todos_data)
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user_name, completed, total))
+        user_data['name'], completed, total))
     for task in completed_tasks:
-        print("\t {}".format(task))
+        print(f"\t{task}")
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+
+    try:
+        user_data, todos_data = fetch_user_todos(employee_id)
+        print_todo_progress(user_data, todos_data)
+    except requests.exceptions.HTTPError as e:
+        print(f"Error: {e.response.status_code} - {e.response.reason}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
