5a1cde9261c56ded907d3352c4a8a6fe79f4a93e
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-23 00:41:49 +0300

    0-gather_data_from_an_API.py update 10

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
 """Gather data from an API and Runs if the module is not imported"""
-from requests import get
-from sys import argv
+import requests
+import sys
 
-if __name__ == '__main__':
-    main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + "/user/{}/todos".format(argv[1])
-    name_url = main_url + "/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+if __name__ == "__main__":
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    user_url = f"{base_url}users/{sys.argv[1]}"
+    todos_url = f"{base_url}todos?userId={sys.argv[1]}"
 
-    todo_num = len(todo_result)
-    todo_complete = len([todo for todo in todo_result
-                         if todo.get("completed")])
-    name = name_result.get("name")
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, todo_complete, todo_num))
-    for todo in todo_result:
-        if (todo.get("completed")):
-            print("\t {}".format(todo.get("title")))
+    try:
+        user_data = requests.get(user_url).json()
+        todos_data = requests.get(todos_url).json()
+
+        user_name = user_data.get('name')
+
+        completed = 0
+        total = 0
+        completed_tasks = []
+
+        for task in todos_data:
+            total += 1
+            if task.get('completed'):
+                completed_tasks.append(task.get('title'))
+                completed += 1
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            user_name, completed, total))
+
+        for task in completed_tasks:
+            print("\t{}".format(task))
+
+    except requests.exceptions.RequestException:
+        pass
+
+    except (KeyError, IndexError):
+        pass
