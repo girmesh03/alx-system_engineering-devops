52500af50042fc80b5d14a1ffb3de42a9b69decd
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-23 00:07:09 +0300

    0-gather_data_from_an_API.py update 5

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,42 +3,14 @@
 import requests
 import sys
 
-
-def fetch_user_todos(employee_id):
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    user_url = f"{base_url}users/{employee_id}"
-    todos_url = f"{base_url}todos"
-
-    response = requests.get(user_url)
-    response.raise_for_status()
-    user_data = response.json()
-
-    response = requests.get(todos_url, params={"userId": employee_id})
-    response.raise_for_status()
-    todos_data = response.json()
-
-    return user_data, todos_data
-
-
-def print_todo_progress(user_data, todos_data):
-    completed_tasks = [task['title']
-                       for task in todos_data if task['completed']]
-    completed = len(completed_tasks)
-    total = len(todos_data)
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_data['name'], completed, total))
-    for task in completed_tasks:
-        print(f"\t{task}")
-
-
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-
-    try:
-        user_data, todos_data = fetch_user_todos(employee_id)
-        print_todo_progress(user_data, todos_data)
-    except requests.exceptions.HTTPError as e:
-        print(f"Error: {e.response.status_code} - {e.response.reason}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    url = "https://jsonplaceholder.typicode.com/"
+    name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
+    tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
+    c_tasks = [task for task in tasks if task.get("completed") is True]
+    titles = [task.get("title") for task in tasks
+              if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name.get("name"), len(c_tasks), len(tasks)))
+    for title in titles:
+        print("\t {}".format(title))
