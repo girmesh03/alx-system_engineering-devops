1dbe791e4986d714ebd724294def64013770b544
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-22 23:50:02 +0300

    0-gather_data_from_an_API.py update 3

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,13 +6,9 @@ import sys
 if __name__ == "__main__":
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_url = f"{base_url}users/{sys.argv[1]}"
-    # print(user_url)
     todos_url = f"{base_url}todos?userId={sys.argv[1]}"
-    # print(todos_url)
     user_name = requests.get(user_url).json()['name']
-    # print(user_name)
     todos_data = requests.get(todos_url).json()
-    # print(todos_data)
     completed = 0
     total = 0
     completed_tasks = []
@@ -21,7 +17,6 @@ if __name__ == "__main__":
         if task['completed']:
             completed_tasks.append(task['title'])
             completed += 1
-
     print("Employee {} is done with tasks({}/{}):".format(
         user_name, completed, total))
     for task in completed_tasks:
