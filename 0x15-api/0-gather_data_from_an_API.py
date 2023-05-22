61063e35b4d9319fe13990567c4b9fdd41418ecd
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-22 23:45:30 +0300

    0-gather_data_from_an_API.py update 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,21 +5,18 @@ import requests
 import sys
 
 
-def get_data_from_api(employee_id):
-    """Gather data from an API"""
-    base_url = 'https://jsonplaceholder.typicode.com/'
+if __name__ == "__main__":
+    """Gather data from an API and Runs if the module is not imported"""
 
+    base_url = 'https://jsonplaceholder.typicode.com/'
     user_url = f"{base_url}users/{sys.argv[1]}"
     # print(user_url)
     todos_url = f"{base_url}todos?userId={sys.argv[1]}"
     # print(todos_url)
-
     user_name = requests.get(user_url).json()['name']
     # print(user_name)
-
     todos_data = requests.get(todos_url).json()
     # print(todos_data)
-
     completed = 0
     total = 0
     completed_tasks = []
@@ -33,13 +30,3 @@ def get_data_from_api(employee_id):
         user_name, completed, total))
     for task in completed_tasks:
         print("\t {}".format(task))
-
-
-if __name__ == "__main__":
-    """Runs if the module is not imported"""
-
-    if len(sys.argv) < 2:
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        exit()
-    else:
-        get_data_from_api(sys.argv[1])
