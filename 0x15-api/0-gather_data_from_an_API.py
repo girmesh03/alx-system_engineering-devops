a570ada6bd7f19f5802fc8e8cf323de6a3ce7c17
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-23 00:21:52 +0300

    0-gather_data_from_an_API.py update 9

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """Gather data from an API and Runs if the module is not imported"""
-import requests
-import sys
+from requests import get
+from sys import argv
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
-    tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
-    c_tasks = [task for task in tasks if task.get("completed") is True]
-    titles = [task.get("title")
-              for task in tasks if task.get("completed") is True]
+if __name__ == '__main__':
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
+
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
+                         if todo.get("completed")])
+    name = name_result.get("name")
     print("Employee {} is done with tasks({}/{}):"
-          .format(name.get("name"), len(c_tasks), len(tasks)))
-    for title in titles:
-        print("\t {}".format(title))
+          .format(name, todo_complete, todo_num))
+    for todo in todo_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
