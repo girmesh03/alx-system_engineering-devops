660905f064ab3f8e89b431250df608329bfcbfda
girmesh03 <girmachewzewdie03@gmail.com>
2023-05-23 04:31:38 +0300

    2-export_to_JSON.py update 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Exports the data retrieved from the JSONPlaceholder API to JSON format"""
+"""Exports to-do list data for an employee to JSON format"""
+
 import json
 import requests
 import sys
@@ -7,18 +8,14 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(user_id)).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(user_id)).json()
-
-    username = user.get("username")
-    tasks = []
-    for task in todo:
-        tasks.append({"task": task.get("title"),
-                      "completed": task.get("completed"),
-                      "username": username})
-    json_dict = {user_id: tasks}
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump(json_dict, jsonfile)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get("{}users/{}".format(url, user_id)).json()
+    tasks = requests.get("{}users/{}/todos".format(url, user_id)).json()
+    data_dict = {
+        user_id: [{"task": task.get("title"),
+                   "completed": task.get("completed"),
+                   "username": user.get("username")} for task in tasks]
+    }
+    # open a new JSON file in write mode
+    with open("{}.json".format(user_id), "w", newline="") as file:
+        json.dump(data_dict, file)
