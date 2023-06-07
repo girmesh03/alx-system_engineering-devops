1795d04e15672e10e32794d5b4bf90d3ad47e307
girmesh03 <girmachewzewdie03@gmail.com>
2023-06-07 03:58:34 +0300

    100-count.py update 2

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -10,35 +10,25 @@ Javascript should count as javascript, but java should not).
 import requests
 
 
-def count_words(subreddit, word_list, after=None, word_dict={}):
+def count_words(subreddit, word_list, array=None, after=""):
     """parses the title of all hot articles, and
     prints a sorted count of given keywords"""
-
+    if array is None:
+        array = [0] * len(word_list)
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     params = {'after': after}
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
-    if response.status_code == 200:
-        results = response.json().get("data")
-        after = results.get("after")
-
-        for child in results.get("children"):
-            title = child.get("data").get("title")
-            for word in word_list:
-                ocurrences = title.lower().split().count(word.lower())
-                if ocurrences > 0:
-                    if word in word_dict:
-                        word_dict[word] += ocurrences
-                    else:
-                        word_dict[word] = ocurrences
-
-        if after is not None:
-            return count_words(subreddit, word_list, after, word_dict)
-        else:
-            if len(word_dict) == 0:
-                return
-            for key, value in sorted(word_dict.items(),
-                                     key=lambda item: item[1],
-                                     reverse=True):
-                print("{}: {}".format(key.lower(), value))
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        return
+    for post in response.json().get('data').get('children'):
+        title = post.get('data').get('title')
+        for i in range(len(word_list)):
+            array[i] += title.lower().split(' ').count(word_list[i].lower())
+    after = response.json().get('data').get('after')
+    if after is None:
+        for i in range(len(word_list)):
+            if array[i] != 0:
+                print("{}: {}".format(word_list[i], array[i]))
+        return
+    return count_words(subreddit, word_list, array, after)
