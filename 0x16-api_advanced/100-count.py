e82d114a642402b7aa5f783c34b9de276d5989c2
girmesh03 <girmachewzewdie03@gmail.com>
2023-06-07 03:53:58 +0300

    100-count.py updated

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -17,23 +17,28 @@ def count_words(subreddit, word_list, after=None, word_dict={}):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
     params = {'after': after}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code != 200:
-        return None
-    for post in response.json().get('data').get('children'):
-        title = post.get('data').get('title')
-        for word in word_list:
-            if word.lower() in title.lower():
-                if word.lower() not in word_dict:
-                    word_dict[word.lower()] = 1
-                else:
-                    word_dict[word.lower()] += 1
-    after = response.json().get('data').get('after')
-    if after is None:
-        if len(word_dict) == 0:
-            return
-        for key, value in sorted(word_dict.items(),
-                                 key=lambda x: (-x[1], x[0])):
-            print("{}: {}".format(key, value))
-        return
-    return count_words(subreddit, word_list, after, word_dict)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        after = results.get("after")
+
+        for child in results.get("children"):
+            title = child.get("data").get("title")
+            for word in word_list:
+                ocurrences = title.lower().split().count(word.lower())
+                if ocurrences > 0:
+                    if word in word_dict:
+                        word_dict[word] += ocurrences
+                    else:
+                        word_dict[word] = ocurrences
+
+        if after is not None:
+            return count_words(subreddit, word_list, after, word_dict)
+        else:
+            if len(word_dict) == 0:
+                return
+            for key, value in sorted(word_dict.items(),
+                                     key=lambda item: item[1],
+                                     reverse=True):
+                print("{}: {}".format(key.lower(), value))
